b95d6d53db61786440f237141efce1b9b07e4851
paipaishuai-create <paipaishuai@gmail.com>
2026-03-30 21:31:00 +0800

    Add ClientSetup class for storage silo

---
+++ src/main/java/uk/binarycraft/storagesilo/ClientSetup.java
@@ -0,0 +1,17 @@
+package uk.binarycraft.storagesilo;
+
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Reference.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ClientSetup {
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            MenuScreens.register(ModMenuTypes.STORAGE_SILO_MENU.get(), StorageSiloScreen::new);
+        });
+    }
+}
