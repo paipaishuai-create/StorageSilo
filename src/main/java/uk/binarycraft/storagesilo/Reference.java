5959558333ee378064c9b1d154b5958251e3c804
paipaishuai-create <paipaishuai@gmail.com>
2026-03-30 20:37:18 +0800

    Update Reference.java with storage silo information for version 1.20.1-REWRITE

--- src/main/java/uk/binarycraft/storagesilo/Reference.java
+++ src/main/java/uk/binarycraft/storagesilo/Reference.java
@@ -1,10 +1,7 @@
 package uk.binarycraft.storagesilo;
 
-public class Reference
-{
-
-	public static final String MODNAME = "StorageSilo";
-	public static final String MODID = "storagesilo";
-	public static final String VERSION = "1.2.2";
-
+public class Reference {
+    public static final String MODID = "storagesilo";
+    public static final String NAME = "Storage Silo";
+    public static final String VERSION = "1.20.1-REWRITE";
 }
